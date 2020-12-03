3d4824fd853e2941fa88c8c420bee846fda0ce6b
abdelrhman <abdelrhmanm525@gmail.com>
2020-12-03 21:49:12 +0200

    a

--- server.js
+++ server.js
@@ -1,17 +1,59 @@
+// const express = require('express');
+// const path = require('path');
+// const history = require('connect-history-api-fallback');
+
+// const app = express();
+
+// app.use(logger('dev'));
+
+// app.use('/', express.static(path.join(__dirname, 'dist')));
+server.use('/dist', express.static(path.join(__dirname, './dist')));
+
+// var port = process.env.PORT || 8080;
+// app.listen(port);
+// console.log('server started '+ port);
+
+//server.js
 const express = require('express');
+const server = express();
+const fs = require('fs');
 const path = require('path');
-const history = require('connect-history-api-fallback');
+//obtain bundle
+const bundle =  require('./dist/server.bundle.js');
+//get renderer from vue server renderer
+const renderer = require('vue-server-renderer').createRenderer({
+  //set template
+  template: fs.readFileSync('./index.html', 'utf-8')
+});
 
-const app = express();
 
-app.use(logger('dev'));
+//start server
+server.get('*', (req, res) => { 
+    
+  bundle.default({ url: req.url }).then((app) => {    
+    //context to use as data source
+    //in the template for interpolation
+    const context = {
+      title: 'Vue JS - Server Render',
+      meta: `
+        <meta description="vuejs server side render">
+      `
+    };
 
-app.get('/api', (req, res) => res.send('Hello World!'));
-app.use(history({
-  verbose: true
-}));
-app.use('/', express.static(path.join(__dirname, 'dist')));
+    renderer.renderToString(app, context, function (err, html) {   
+      if (err) {
+        if (err.code === 404) {
+          res.status(404).end('Page not found')
+        } else {
+          res.status(500).end('Internal Server Error')
+        }
+      } else {
+        res.end(html)
+      }
+    });        
+  }, (err) => {
+    console.log(err);
+  });  
+});  
 
-var port = process.env.PORT || 8080;
-app.listen(port);
-console.log('server started '+ port);
+server.listen(8080);
