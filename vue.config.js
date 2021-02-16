37c22048900a4329e3b9b8d16db45c8fb98cc6c2
abdelrhman <abdelrhmanm525@gmail.com>
2021-02-16 21:14:40 +0200

    Buliding

--- vue.config.js
+++ vue.config.js
@@ -12,70 +12,62 @@ module.exports = {
     }
   },
   pwa: {
-    name: 'AMS STUDIO',
+    name: 'AMS-STUDIO',
     themeColor: '#ff8429',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    pwa: {
-      manifestOptions: {
-        name: "AMS STUDIO",
-        short_name: "AMS STUDIO",
-        start_url: "/index.html",
-        display: "standalone",
-        theme_color: "#ff8429",
-        icons: [
-          {
-            src: "/src/assets/pwa_icons/512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "App Icon",
-          },
-          {
-            src: "/src/assets/pwa_icons/384.png",
-            sizes: "384x384",
-            type: "image/png",
-            purpose: "App Icon",
-          },
-          {
-            src: "/src/assets/pwa_icons/256.png",
-            sizes: "256x256",
-            type: "image/png",
-            purpose: "App Icon",
-          },
-          {
-            src: "/src/assets/pwa_icons/192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "App Icon",
-          },
-          {
-            src: "/src/assets/pwa_icons/144.png",
-            sizes: "144x144",
-            type: "image/png",
-            purpose: "App Icon",
-          },
-          {
-            src: "/src/assets/pwa_icons/96.png",
-            sizes: "96x96",
-            type: "image/png",
-            purpose: "App Icon",
-          },
-          {
-            src: "/src/assets/pwa_icons/48.png",
-            sizes: "48x48",
-            type: "image/png",
-            purpose: "App Icon",
-          },
-        ],
+    icons: [
+      {
+        src: "/src/assets/pwa_icons/512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "App Icon",
       },
-      // configure the workbox plugin
-      workboxPluginMode: 'InjectManifest',
-      workboxOptions: {
-        // swSrc is required in InjectManifest mode.
-        swSrc: 'public/service-worker.js',
-        // ...other Workbox options...
-      }
+      {
+        src: "/src/assets/pwa_icons/384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "App Icon",
+      },
+      {
+        src: "/src/assets/pwa_icons/256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "App Icon",
+      },
+      {
+        src: "/src/assets/pwa_icons/192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "App Icon",
+      },
+      {
+        src: "/src/assets/pwa_icons/144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "App Icon",
+      },
+      {
+        src: "/src/assets/pwa_icons/96.png",
+        sizes: "96x96",
+        type: "image/png",
+        purpose: "App Icon",
+      },
+      {
+        src: "/src/assets/pwa_icons/48.png",
+        sizes: "48x48",
+        type: "image/png",
+        purpose: "App Icon",
+      },
+    ],
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'public/service-worker.js',
+      // ...other Workbox options...
     }
   }
+}
 
